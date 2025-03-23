57161f5f42ed433f03840d931278a78fb391e0c1
anibev3 <anibev3@gmail.com>
2025-03-23 05:07:19 +0000

    wip

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -1,12 +1,12 @@
 // lib/config.ts
 export const config = {
     googleMaps: {
-        apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '',
-        url: process.env.NEXT_PUBLIC_GOOGLE_MAPS_URL || 'https://maps.googleapis.com/maps/api/directions/json?'
+        apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+        url: process.env.NEXT_PUBLIC_GOOGLE_MAPS_URL
     },
     api: {
-        baseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://save-road.roomcodetraining.com/api',
-        iconUrl: process.env.NEXT_PUBLIC_ICON_URL || 'https://save-road.roomcodetraining.com/storage/icon/',
-        photoUrl: process.env.NEXT_PUBLIC_PHOTO_URL || 'https://save-road.roomcodetraining.com/storage/photo/'
+        baseUrl: process.env.NEXT_PUBLIC_API_URL,
+        iconUrl: process.env.NEXT_PUBLIC_ICON_URL,
+        photoUrl: process.env.NEXT_PUBLIC_PHOTO_URL
     }
 };
